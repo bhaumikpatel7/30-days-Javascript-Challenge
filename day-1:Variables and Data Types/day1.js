97ffbb1eb3b6d497971ccc33ea9c50ba0f2ec47a
Bhaumik Patel <72443943+bhaumikpatel7@users.noreply.github.com>
2024-07-18 03:40:17 +0000

    end of the first day challenge

--- day-1:Variables and Data Types/day1.js
+++ day-1:Variables and Data Types/day1.js
@@ -1,8 +1,47 @@
 //task:1
 var num = 1;
+
+//task-2
 let name = "Bhaumik";
 
+//task-3
 const booleanNumber = true;
-
 console.log(booleanNumber);
-console.log(name);
+
+//task-4
+let number = 1;
+let str = "Bhaumik"
+let bool = false
+let arr = [1,2,3,4,5];
+let obj1 = {number:1,numb:2}
+
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(arr));
+console.log(typeof(obj1));
+
+
+//task-5
+
+
+function newNumberWithLet(){
+    let num1=2222;
+    console.log("before num1 = "+ num1);
+    num1 = 2000;
+    console.log("after reassignment num1 = "+ num1);
+}
+
+newNumberWithLet()
+
+//task-6
+
+function newNumberWithConst(){
+    const num2=2222;
+    console.log("before num1 = "+ num2);
+    num2 = 2000;
+    console.log("after reassignment num1 = "+ num2);
+    //TypeError TypeError: Assignment to constant variable.
+}
+
+//newNumberWithConst()
