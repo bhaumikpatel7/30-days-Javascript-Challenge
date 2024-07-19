2797b9188de3fce2a108a340eb9c64fbd446770c
Bhaumik Patel <72443943+bhaumikpatel7@users.noreply.github.com>
2024-07-19 02:38:18 +0000

    day-2 completed

---
+++ Day-2:Operators/day2.js
@@ -0,0 +1,42 @@
+//day-2
+
+//task-1
+
+//addition
+
+const  a = 10;
+const b = 20;
+
+console.log("a + b is " , a+b);
+
+
+//task -2
+console.log("a + b is " , a-b);
+
+//task-3
+console.log("a + b is " , a*b);
+
+//task-4
+console.log("a + b is " , a/b);
+
+//task-5
+
+function findRemainder(n){
+  console.log(n%2);
+}
+findRemainder(3);
+
+//task-6
+let c = 30;
+console.log(c+=2);
+//task-8
+let d=60;
+console.log(d-=2);
+
+
+//task-9, task-14
+let n=3;
+n>0?console.log("positive"):console.log("negative")
+
+//task-10
+//task-11
